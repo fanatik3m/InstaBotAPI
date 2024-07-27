aa0550f0e80a9ed52d706f6b925c6dc5b88699f1
Станислав <stanislavskuratovich37@gmail.com>
2024-07-27 16:25:44 +0300

    added for deploy

--- src/main.py
+++ src/main.py
@@ -2,6 +2,7 @@ import os
 import sys
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from auth.router import router as auth_router
 from groups.router import group_router, client_router
@@ -10,6 +11,17 @@ sys.path.insert(1, os.path.join(sys.path[0], '..'))
 
 app = FastAPI(
     title='InstaBot_API'
+)
+
+# add frontend when its location will be known
+origins = ['*']
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=['*'],
+    allow_headers=['*'],
 )
 
 app.include_router(auth_router)
